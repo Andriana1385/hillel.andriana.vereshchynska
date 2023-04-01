c53b15e69fe10815dee9677dfa9817286e2b4fb5
Andriana1385 <nanochka1385@gmail.com>
2023-04-01 11:06:56 +0300

    Logical new

--- java_qa_manual_lectures-main/src/com/hillel/qa/java/logical_operators/homework/HomeWork1.java
+++ java_qa_manual_lectures-main/src/com/hillel/qa/java/logical_operators/homework/HomeWork1.java
@@ -14,10 +14,11 @@ public class HomeWork1 {
      * </ol>
      */
     public static void main(String[] args) {
-        int age = 16;
-        boolean hasInsurance = false;
+        int age = 20;
+        int minAge = 16;
+        boolean hasInsurance = true;
         boolean hasMedicalpermission = true;
-        boolean isProvideDriverLicense = age >= age && (!hasInsurance) && hasMedicalpermission;
+        boolean isProvideDriverLicense = age >= minAge && hasInsurance && hasMedicalpermission;
         System.out.println(isProvideDriverLicense);
     }
 }
